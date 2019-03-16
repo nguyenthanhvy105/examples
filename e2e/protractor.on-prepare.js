01ec11ab236ee5156fb4f9fc5ddf83ae1e3a4951
vy.nguyen <nguyenthanhvy105@gmail.com>
2019-03-16 12:06:49 +0700

    Improves gets the translations from the server

--- e2e/protractor.on-prepare.js
+++ e2e/protractor.on-prepare.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
   // is the configured.
   const portFlag = config.server.endsWith('prodserver') ? '-p' : '-port';
   return protractorUtils.runServer(config.workspace, config.server, portFlag, [])
-      .then(serverSpec => {
+      .then((serverSpec) => {
         const serverUrl = `http://localhost:${serverSpec.port}`;
         protractor.browser.baseUrl = serverUrl;
       });
