bb92795d54740d959960d34b34c3afe4cb3b7b97
vy.nguyen <nguyenthanhvy105@gmail.com>
2019-11-11 16:27:08 +0700

    Update scripts

--- gulpfile.js
+++ gulpfile.js
@@ -87,13 +87,14 @@ function copyChunkToBuildFolder(callback) {
             .pipe(gulpRename(updatePath))
             .pipe(gulp.dest(buildDir + '/css')).on('end', resolve);
     }));
-    promises.push(new Promise(resolve => {
-        gulp.src(['dist/ng/**/*.properties'])
+
+/*    promises.push(new Promise(resolve => {
+        gulp.src(['dist/ng/!**!/!*.properties'])
             .pipe(gulpRename(function(path){
                 path.dirname = '';
             }))
             .pipe(gulp.dest(buildDir + '/message')).on('end', resolve);
-    }));
+    }));*/
     Promise.all(promises).then(callback);
 
     function updatePath(path) {
