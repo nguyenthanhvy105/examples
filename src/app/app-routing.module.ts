d12999c3dd591be2812b913e76f65f7af57e08d1
vy.nguyen <nguyenthanhvy105@gmail.com>
2021-05-29 22:59:57 +0700

    Update config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,11 @@ import {HomeComponent} from "./pages/home/home.component";
 const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'select', loadChildren: './pages/select/test-select.module#TestSelectModule'},
-    {path: 'grid', loadChildren: './pages/grid/test-grid.module#TestGridModule'},
-    {path: 'list-view', loadChildren: './pages/list-view/test-list-view.module#TestListViewModule'},
-    {path: 'form', loadChildren: './pages/form/test-form.module#TestFormModule'},
-    {path: 'themes', loadChildren: './pages/themes/themes.module#ThemesModule'}
-
+    {path: 'select', loadChildren: () => import('./pages/select/test-select.module').then(m => m.TestSelectModule)},
+    {path: 'grid', loadChildren: () => import('./pages/grid/test-grid.module').then(m => m.TestGridModule)},
+    {path: 'list-view', loadChildren: () => import('./pages/list-view/test-list-view.module').then(m => m.TestListViewModule)},
+    {path: 'form', loadChildren: () => import('./pages/form/test-form.module').then(m => m.TestFormModule)},
+    {path: 'themes', loadChildren: () => import('./pages/themes/themes.module').then(m => m.ThemesModule)}
 ];
 
 @NgModule({
